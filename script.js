28b94945cefedfe85f8ea257076c165fcac4f88c
Matheus Aparecido <105564721+MatheusAp93@users.noreply.github.com>
2024-08-09 09:55:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const hamburger = document.querySelector('.nav__hamburger');
-  const menu = document.querySelector('.cabecalho__menu');
-
-  hamburger.addEventListener('click', () => {
-    if (menu.classList.contains("nav__menu--active")) {
-      menu.classList.remove("nav__menu--active");
-      menu.style.maxHeight = "0";
-    } else {
-      menu.classList.add("nav__menu--active");
-      menu.style.maxHeight = menu.scrollHeight + "px";
-    }
-  });
-});
 
